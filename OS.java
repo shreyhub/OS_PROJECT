3e85ef3ba2272b717066bd64e5e78217d06bbc07
shryaSS <44334051+shryaSS@users.noreply.github.com>
2019-04-09 04:09:10 +0000

    Add files via upload

---
+++ OS.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+interface Input{
+void input();
+void task();
+}
+
+public class OS{
+public static void  main(String[] aa){
+
+
+System.out.printf("Layout of the  main class");
+
+}}
+
+
